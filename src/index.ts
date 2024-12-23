da78eacca950a5220401f39820e42a40bb8c681d
aquapi <aquaplmc@gmail.com>
2024-12-23 16:48:18 +0700

    Hmm

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,24 @@ export type LazyString = string | LazyString[];
 export interface CompilerState {
   declarationBuilders: LazyString[];
   externalValues: any[];
-  globalBuilders: [name: string, LazyString][];
+  globalBuilders: Map<string, LazyString>;
 }
 
 // eslint-disable-next-line
 export const getExternalKeys = (state: CompilerState): string[] => state.externalValues.map((_, idx) => 'f' + (idx + 1));
 
 // eslint-disable-next-line
-export const getDeclarations = (state: CompilerState): string =>
+export const getDeclarations = (state: CompilerState): string => {
   // @ts-expect-error TSC dies lol
   // eslint-disable-next-line
-  state.declarationBuilders.flat(Infinity).map((str, idx) => 'var d' + (idx + 1) + '=' + str + ';').join('') +
-  // eslint-disable-next-line
-  state.globalBuilders.map((pair) => 'var ' + pair[0] + '=' + (typeof pair[1] === 'string' ? pair[1] : pair[1].flat(Infinity).join(''))).join('');
+  let str = state.declarationBuilders.flat(Infinity).map((str, idx) => 'var d' + (idx + 1) + '=' + str + ';').join('');
+
+  state.globalBuilders.forEach((val, key) => {
+    str += 'var ';
+    str += key;
+    str += '=';
+    str += typeof val === 'string' ? val : val.flat(Infinity).join('');
+  });
+
+  return str;
+};
