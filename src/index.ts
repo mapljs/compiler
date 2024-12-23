635b7785743fcb8452b7b2269d728a7614bef66b
aquapi <aquaplmc@gmail.com>
2024-12-23 16:38:51 +0700

    Add lazy support

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
+export type LazyString = string | LazyString[];
+
 export interface CompilerState {
-  declarationBuilders: string[];
+  declarationBuilders: LazyString[];
   externalValues: any[];
+  globalBuilders: Record<string, LazyString>;
 }
 
 // eslint-disable-next-line
 export const getExternalKeys = (state: CompilerState): string[] => state.externalValues.map((_, idx) => 'f' + (idx + 1));
 
 // eslint-disable-next-line
-export const getDeclarations = (state: CompilerState): string => state.declarationBuilders.map((str, idx) => 'var d' + (idx + 1) + '=' + str + ';').join('');
+export const getDeclarations = (state: CompilerState): string =>
+  // @ts-expect-error TSC dies lol
+  // eslint-disable-next-line
+  state.declarationBuilders.flat(Infinity).map((str, idx) => 'var d' + (idx + 1) + '=' + str + ';').join('') + Object.entries(
+    state.declarationBuilders
+  // eslint-disable-next-line
+  ).map((pair) => 'var ' + pair[0] + '=' + (typeof pair[1] === 'string' ? pair[1] : pair[1].flat(Infinity).join('')));
